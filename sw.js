e416966a013a48ada8a03c04988c7ecea25bdafa
Roham60 <employment.kerman@gmail.com>
2026-06-24 20:12:55 +0330

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,28 @@
+
+const CACHE_NAME = 'mabda-v3';
+const FILES = [
+  './index.html',
+  './manifest.json',
+  './logo.png',
+  './qrcode.min.js',
+  './jspdf.umd.min.js',
+  './xlsx.full.min.js',
+  './html2canvas.min.js',
+  './html5-qrcode.min.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).catch(()=>caches.match('./MABDA-TECHNICAL.html')))
+  );
+});
